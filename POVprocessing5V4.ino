b9584b996e7a6ab71db030bc21082c2535a7bb7a
nick11235 <florian_nick@windowslive.com>
2019-10-23 12:47:54 +0300

    Add files via upload

---
+++ POVprocessing5V4.ino
@@ -0,0 +1,446 @@
+     /*
+ *  Persistance of Vision
+ */
+byte n9[] = {1, 1, 1, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 1, 1, 1, 1, 1, 1, 1};
+byte n8[] = {0, 1, 1, 0, 1, 1, 1, 0,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  0, 1, 1, 0, 1, 1, 1, 0};
+byte n7[] = {1, 0, 0, 0, 0, 0, 0, 0,  1, 0, 0, 0, 1, 0, 0, 0,  1, 0, 0, 0, 1, 0, 0, 0,  1, 0, 0, 1, 1, 1, 1, 1,  1, 1, 1, 0, 1, 0, 0, 0};
+byte n6[] = {1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 1, 1, 1};
+byte n5[] = {1, 1, 1, 1, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 1, 1, 1};
+byte n4[] = {1, 1, 1, 1, 1, 0, 0, 0,  0, 0, 0, 0, 1, 0, 0, 0,  0, 0, 0, 0, 1, 0, 0, 0,  0, 0, 0, 0, 1, 0, 0, 0,  1, 1, 1, 1, 1, 1, 1, 1};
+byte n3[] = {1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 1, 1, 1, 1, 1, 1, 1};
+byte n2[] = {1, 0, 0, 0, 0, 0, 1, 1,  1, 0, 0, 0, 0, 1, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  0, 1, 1, 0, 0, 0, 0, 1};
+byte n1[] = {0, 0, 1, 0, 0, 0, 0, 0,  0, 1, 0, 0, 0, 0, 0, 0,  1, 1, 1, 1, 1, 1, 1, 1,  0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0};
+byte n0[] = {1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 0, 0, 0, 0, 1,  1, 0, 0, 0, 0, 0, 0, 1,  1, 0, 0, 0, 0, 0, 0, 1,  1, 1, 1, 1, 1, 1, 1, 1};
+
+byte _[] = {0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0};
+byte comma[] = {0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 0, 0, 1, 0,  0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0};
+byte exclamation[] = {0, 0, 0, 0, 0, 0, 0, 0,  1, 1, 1, 1, 1, 0, 0, 1,  0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0};
+byte question[] = {0, 0, 0, 0, 0, 0, 0, 0,  0, 1, 0, 0, 0, 0, 0, 0,  1, 0, 0, 1, 1, 0, 0, 1,  0, 1, 1, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0};
+byte smile[] = {1, 1, 0, 0, 0, 0, 1, 0,   1, 1, 0, 0, 0, 0, 0, 1,   0, 0, 0, 1, 1, 0, 0, 1,   1, 1, 0, 0, 0, 0, 0, 1,   1, 1, 0, 0, 0, 0, 1, 0};
+byte A[] = {1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 1, 0, 0, 0, 0,  1, 0, 0, 1, 0, 0, 0, 0,  1, 0, 0, 1, 0, 0, 0, 0,  1, 1, 1, 1, 1, 1, 1, 1};
+byte B[] = {1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  0, 1, 1, 0, 1, 1, 1, 0};
+byte C[] = {0, 0, 1, 1, 1, 1, 0, 0,  0, 1, 0, 0, 0, 0, 1, 0,  1, 0, 0, 0, 0, 0, 0, 1,  1, 0, 0, 0, 0, 0, 0, 1,  1, 0, 0, 0, 0, 0, 0, 1};
+byte D[] = {1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 0, 0, 0, 0, 1,  1, 0, 0, 0, 0, 0, 0, 1,  0, 1, 0, 0, 0, 0, 1, 0,  0, 0, 1, 1, 1, 1, 0, 0};
+byte E[] = {1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1};
+byte F[] = {1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 1, 0, 0, 0, 0,  1, 0, 0, 1, 0, 0, 0, 0,  1, 0, 0, 1, 0, 0, 0, 0,  1, 0, 0, 1, 0, 0, 0, 0};
+byte G[] = {0, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 0, 0, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 1, 1, 0};
+byte H[] = {1, 1, 1, 1, 1, 1, 1, 1,  0, 0, 0, 0, 1, 0, 0, 0,  0, 0, 0, 0, 1, 0, 0, 0,  0, 0, 0, 0, 1, 0, 0, 0,  1, 1, 1, 1, 1, 1, 1, 1};
+byte I[] = {0, 0, 0, 0, 0, 0, 0, 0,  1, 0, 0, 0, 0, 0, 0, 1,  1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 0, 0, 0, 0};
+byte J[] = {0, 0, 0, 0, 0, 1, 1, 0,  0, 0, 0, 0, 1, 0, 0, 1,  0, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 0, 0, 0, 1,  1, 1, 1, 1, 1, 1, 1, 0};
+byte K[] = {1, 1, 1, 1, 1, 1, 1, 1,  0, 0, 0, 1, 1, 0, 0, 0,  0, 0, 1, 0, 0, 1, 0, 0,  0, 1, 0, 0, 0, 0, 1, 0,  1, 0, 0, 0, 0, 0, 0, 1};
+byte L[] = {1, 1, 1, 1, 1, 1, 1, 1,  0, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 0, 0, 0, 1};
+byte M[] = {1, 1, 1, 1, 1, 1, 1, 1,  0, 1, 0, 0, 0, 0, 0, 0,  0, 0, 1, 0, 0, 0, 0, 0,  0, 1, 0, 0, 0, 0, 0, 0,  1, 1, 1, 1, 1, 1, 1, 1};
+byte N[] = {1, 1, 1, 1, 1, 1, 1, 1,  0, 0, 1, 0, 0, 0, 0, 0,  0, 0, 0, 1, 1, 0, 0, 0,  0, 0, 0, 0, 0, 1, 0, 0,  1, 1, 1, 1, 1, 1, 1, 1};
+byte O[] = {0, 1, 1, 1, 1, 1, 1, 0,  1, 0, 0, 0, 0, 0, 0, 1,  1, 0, 0, 0, 0, 0, 0, 1,  1, 0, 0, 0, 0, 0, 0, 1,  0, 1, 1, 1, 1, 1, 1, 0};
+byte P[] = {1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 1, 0, 0, 0, 0,  1, 0, 0, 1, 0, 0, 0, 0,  1, 0, 0, 1, 0, 0, 0, 0,  0, 1, 1, 0, 0, 0, 0, 0};
+byte Q[] = {0, 1, 1, 1, 1, 1, 1, 0,  1, 0, 0, 0, 0, 0, 0, 1,  1, 0, 0, 0, 0, 1, 0, 1,  0, 1, 1, 1, 1, 1, 1, 0,  0, 0, 0, 0, 0, 0, 0, 1};
+byte R[] = {1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 1, 1, 0, 0, 0,  1, 0, 0, 1, 0, 1, 0, 0,  1, 0, 0, 1, 0, 0, 1, 0,  0, 1, 1, 0, 0, 0, 0, 1};
+byte S[] = {0, 1, 1, 1, 0, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 1, 1, 1, 0};
+byte T[] = {1, 0, 0, 0, 0, 0, 0, 0,  1, 0, 0, 0, 0, 0, 0, 0,  1, 1, 1, 1, 1, 1, 1, 1,  1, 0, 0, 0, 0, 0, 0, 0,  1, 0, 0, 0, 0, 0, 0, 0};
+byte U[] = {1, 1, 1, 1, 1, 1, 1, 0,  0, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 0, 0, 0, 1,  1, 1, 1, 1, 1, 1, 1, 0};
+byte V[] = {1, 1, 1, 1, 1, 1, 0, 0,  0, 0, 0, 0, 0, 0, 1, 0,  0, 0, 0, 0, 0, 0, 0, 1,  0, 0, 0, 0, 0, 0, 1, 0,  1, 1, 1, 1, 1, 1, 0, 0};
+byte W[] = {1, 1, 1, 1, 1, 1, 1, 1,  0, 0, 0, 0, 0, 0, 1, 0,  0, 0, 0, 0, 0, 1, 0, 0,  0, 0, 0, 0, 0, 0, 1, 0,  1, 1, 1, 1, 1, 1, 1, 1};
+byte X[] = {1, 1, 0, 0, 0, 0, 1, 1,  0, 0, 1, 0, 0, 1, 0, 0,  0, 0, 0, 1, 1, 0, 0, 0,  0, 0, 1, 0, 0, 1, 0, 0,  1, 1, 0, 0, 0, 0, 1, 1};
+byte Y[] = {1, 1, 0, 0, 0, 0, 0, 0,  0, 0, 1, 0, 0, 0, 0, 0,  0, 0, 0, 1, 1, 1, 1, 1,  0, 0, 1, 0, 0, 0, 0, 0,  1, 1, 0, 0, 0, 0, 0, 0};
+byte Z[] = {1, 0, 0, 0, 0, 1, 1, 1,  1, 0, 0, 0, 1, 0, 0, 1,  1, 0, 0, 1, 0, 0, 0, 1,  1, 0, 1, 0, 0, 0, 0, 1,  1, 1, 0, 0, 0, 0, 0, 1};
+
+int k = -1;
+byte h = 0;
+float letterSpace;
+float dotTime;
+char income;
+char* alfa[] = {'_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'};
+
+void setup()
+{
+  
+  Serial.begin(115200);
+  //Serial.begin(9600);
+  // setting the ports of the leds to OUTPUT
+  for ( byte i = 2; i < 10 ; i++ )
+  {
+    pinMode(i, OUTPUT);
+  }
+
+  // defining the space between the letters (ms)
+  letterSpace = 2; // 3
+  // defining the time dots appear (ms)
+  dotTime = 1; // 2
+
+  /*for ( byte i = 0; i < 20 ; i++ )
+  {
+    alfa[i] = "_";
+  }*/
+
+}
+
+void printLetter(char letter[])
+{
+  byte y;
+
+  // printing the first y row of the letter
+  for (y = 0; y < 8; y++)
+  {
+    digitalWrite(y + 2, letter[y]);
+  }
+  delay(dotTime);
+  // printing the second y row of the letter
+  for (y = 0; y < 8; y++)
+  {
+    digitalWrite(y + 2, letter[y + 8]);
+  }
+  delay(dotTime);
+  // printing the third y row of the letter
+  for (y = 0; y < 8; y++)
+  {
+    digitalWrite(y + 2, letter[y + 16]);
+  }
+  delay(dotTime);
+  for (y = 0; y < 8; y++) {
+    digitalWrite(y + 2, letter[y + 24]);
+  }
+  delay(dotTime);
+  for (y = 0; y < 8; y++) {
+    digitalWrite(y + 2, letter[y + 32]);
+  }
+  delay(dotTime);
+
+  // printing the space between the letters
+  for (y = 0; y < 8; y++)
+  {
+    digitalWrite(y + 2, 0);
+  }
+  delay(letterSpace);
+}
+
+
+void loop()
+{
+  if (Serial.available()) {
+    income = Serial.read();
+    switch (income) {
+
+      case 'A':
+        //printLetter(A);
+        k++;
+        alfa[k] = 'A';        
+        break;
+      case 'B':
+        k++;
+        alfa[k] = 'B';        
+        break;
+      case 'C':
+        k++;
+        alfa[k] = 'C';        
+        break;
+      case 'D':
+        k++;
+        alfa[k] = 'D';
+        break;
+      case 'E':
+        k++;
+        alfa[k] = 'E';
+        break;
+      case 'F':
+        k++;
+        alfa[k] = 'F';
+        break;
+      case 'G':
+        k++;
+        alfa[k] = 'G';
+        break;
+      case 'H':
+        k++;
+        alfa[k] = 'H';
+        break;
+      case 'I':
+        k++;
+        alfa[k] = 'I';
+        break;
+      case 'J':
+        k++;
+        alfa[k] = 'J';
+        break;
+      case 'K':
+        k++;
+        alfa[k] = 'K';        
+        break;
+      case 'L':
+        k++;
+        alfa[k] = 'L';        
+        break;
+      case 'M':
+        k++;
+        alfa[k] = 'M';        
+        break;
+      case 'N':
+        k++;
+        alfa[k] = 'N';        
+        break;
+      case 'O':
+        k++;
+        alfa[k] = 'O';        
+        break;
+      case 'P':
+        k++;
+        alfa[k] = 'P';        
+        break;
+
+      case 'Q':
+        k++;
+        alfa[k] = 'Q';
+        break;
+      case 'R':
+        k++;
+        alfa[k] = 'R';        
+        break;
+      case 'S':
+        k++;
+        alfa[k] = 'S';        
+        break;
+      case 'T':
+        k++;
+        alfa[k] = 'T';        
+        break;
+      case 'U':
+        k++;
+        alfa[k] = 'U';        
+        break;
+      case 'V':
+        k++;
+        alfa[k] = 'V';        
+        break;
+      case 'W':
+        k++;
+        alfa[k] = 'W';        
+        break;
+      case'X':
+        k++;
+        alfa[k] = 'X';        
+        break;
+
+      case 'Y':
+        k++;
+        alfa[k] = 'Y';        
+        break;
+      case'Z':
+        k++;
+        alfa[k] = 'Z';        
+        break;
+      case '0':
+        k++;
+        alfa[k] = "n0";
+        break;
+      case '1':
+        k++;
+        alfa[k] = "n1";        
+        break;
+      case '2':
+        k++;
+        alfa[k] = "n2";        
+        break;
+      case '3':
+        k++;
+        alfa[k] = "n3";        
+        break;
+      case '4':
+        k++;
+        alfa[k] = "n4";        
+        break;
+      case '5':
+        k++;
+        alfa[k] = "n5";        
+        break;
+
+      case '6':
+        k++;
+        alfa[k] = "n6";        
+        break;
+      case '7':
+        k++;
+        alfa[k] = "n7";        
+        break;
+      case '8':
+        k++;
+        alfa[k] = "n8";        
+        break;
+      case '9':
+        k++;
+        alfa[k] = "n9";        
+        break;
+      case 's':
+        k++;
+        alfa[k] = "_";        
+        break;
+      case 'e':
+        k++;
+        alfa[k] = "exclamation";
+        break;
+      case 'c':
+        k++;
+        alfa[k] = "comma";        
+        break;
+
+      case '/':
+        /*for ( int i = 0; i <= k; i++ ) {
+          alfa[i] = "_";
+        }*/
+        k = -1;
+        h = 0;
+        letterSpace = 1;
+        dotTime = 1;
+        break;
+
+      case 'h':
+        if (h == 0) {
+          h = 1;
+          k = -1;  
+        } else {
+          h = 0;
+        }        
+        break;
+        
+      case 'l':
+        letterSpace+=0.5;    
+        break;
+      case 'm':
+        letterSpace-=0.5;    
+        break;
+
+      case 'n':
+        dotTime+=0.5;
+        break;
+      case 'o':
+        dotTime-=0.5;
+        break;
+      case 'q':
+        k++;
+        alfa[k] = "question";
+        break;
+      case 'p':
+        k++;
+        alfa[k] = "smile";
+        break;
+        
+      default:
+        break;
+    }
+  }
+
+  //Serial.println(k);
+  
+  
+  if (k > -1) {
+    for (int i = 0; i <= k; i++ ) {
+      if(alfa[i] == 'A')
+        printLetter(A);
+      else if(alfa[i] == 'B')
+        printLetter(B);
+      else if(alfa[i] == 'C')
+        printLetter(C);
+      else if(alfa[i] == 'D')
+        printLetter(D);
+      else if(alfa[i] == 'E')
+        printLetter(E);
+      else if(alfa[i] == 'F')
+        printLetter(F);
+      else if(alfa[i] == 'G')
+        printLetter(G);
+      else if(alfa[i] == 'H')
+        printLetter(H);
+      else if(alfa[i] == 'I')
+        printLetter(I);
+      else if(alfa[i] == 'J')
+        printLetter(J);
+      else if(alfa[i] == 'K')
+        printLetter(K);
+      else if(alfa[i] == 'L')
+        printLetter(L);
+      else if(alfa[i] == 'M')
+        printLetter(M);
+      else if(alfa[i] == 'N')
+        printLetter(N);
+      else if(alfa[i] == 'O')
+        printLetter(O);
+      else if(alfa[i] == 'P')
+        printLetter(P);
+      else if(alfa[i] == 'Q')
+        printLetter(Q);
+      else if(alfa[i] == 'R')
+        printLetter(R);
+      else if(alfa[i] == 'S')
+        printLetter(S);
+      else if(alfa[i] == 'T')
+        printLetter(T);
+      else if(alfa[i] == 'U')
+        printLetter(U);
+      else if(alfa[i] == 'V')
+        printLetter(V);
+      else if(alfa[i] == 'W')
+        printLetter(W);
+      else if(alfa[i] == 'X')
+        printLetter(X);
+      else if(alfa[i] == 'Y')
+        printLetter(Y);
+      else if(alfa[i] == 'Z')
+        printLetter(Z);
+      else if(alfa[i] == "n0")
+        printLetter(n0);
+      else if(alfa[i] == "n1")
+        printLetter(n1);
+      else if(alfa[i] == "n2")
+        printLetter(n2);
+      else if(alfa[i] == "n3")
+        printLetter(n3);
+      else if(alfa[i] == "n4")
+        printLetter(n4);
+      else if(alfa[i] == "n5")
+        printLetter(n5);
+      else if(alfa[i] == "n6")
+        printLetter(n6);
+      else if(alfa[i] == "n7")
+        printLetter(n7);
+      else if(alfa[i] == "n8")
+        printLetter(n8);
+      else if(alfa[i] == "n9")
+        printLetter(n9);
+      else if(alfa[i] == "_")
+        printLetter(_);
+      else if(alfa[i] == "exclamation")
+        printLetter(exclamation);
+      else if(alfa[i] == "question")
+        printLetter(question);
+      else if(alfa[i] == "comma")
+        printLetter(comma);
+      else if(alfa[i] == "smile")
+        printLetter(smile);
+      //delay(20);
+    }
+  } else if (h == 1) {
+    printLetter(H);
+    printLetter(E);
+    printLetter(L);
+    printLetter(L);
+    printLetter(O);
+    printLetter(comma);
+    printLetter(_);
+    printLetter(W);
+    printLetter(O);
+    printLetter(R);
+    printLetter(L);
+    printLetter(D);
+    printLetter(exclamation);
+    printLetter(_);
+    printLetter(_);
+    printLetter(_);
+    printLetter(_);
+    printLetter(_);
+    printLetter(_);
+    printLetter(_);
+    printLetter(_);
+    printLetter(_);
+    printLetter(_);
+    printLetter(_);
+  }
+    
+}
